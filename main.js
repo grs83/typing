18781713a7d73d16142920478c37da5fcce820a7
Greg Shamalta <gregshamalta@yahoo.com>
2018-03-02 10:09:40 -0800

    Bug fixed and completed next letter

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+$previus_letter = document.querySelector("#letter__previous");
 $required_letter = document.querySelector("#letter__required");
 $next_letter = document.querySelector("#letter__next");
 $input_text = document.querySelector("#input__text");
 
-var displayLettersArray = [];
+var multipleLettersArray = [];
 var arrayDisplayIndex = 0;
 
 function randomizeLetterArray() {
@@ -40,21 +41,21 @@ function randomizeLetterArray() {
 
 function createMultipleLettersArray() {
   for (var i = 0; i < 3; i++) {
-    displayLettersArray.push(randomizeLetterArray());
+    multipleLettersArray.push(randomizeLetterArray());
   }
 }
 
+randomizeLetterArray();
+createMultipleLettersArray();
+
+var completedLettersArray = _.flatten(multipleLettersArray);
+
 function displayLetters(index) {
-  var letters = displayLettersArray.slice(index, index + 3);
+  var letters = completedLettersArray.slice(index, index + 3);
   return letters;
 }
 
 var letterArray = displayLetters(arrayDisplayIndex);
-$next_letter.textContent = letterArray[2];
+$previus_letter.textContent = letterArray[0];
 $required_letter.textContent = letterArray[1];
-
-console.log(
-  randomizeLetterArray(),
-  createMultipleLettersArray(),
-  displayLettersArray
-);
+$next_letter.textContent = letterArray[2];
